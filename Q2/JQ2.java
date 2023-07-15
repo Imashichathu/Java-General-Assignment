b78a08b160f940ab37359d444e4bf1da8ea16736
Imashichathu <131977756+Imashichathu@users.noreply.github.com>
2023-07-15 21:26:41 +0530

    Add files via upload

---
+++ Q2/JQ2.java
@@ -0,0 +1,11 @@
+package com.mycompany.jq2;
+public class JQ2
+{
+    public static void main(String[] args)
+    {
+        int x = 10 , y = 15 , z = 20 ;
+        int a;
+        a =( x * y * z );
+        System.out.println("Product of x , y & z is "+a);
+    }
+}
