19c2d523e2ec335746e7cb9f4d772691c0dac2e2
Imashichathu <131977756+Imashichathu@users.noreply.github.com>
2023-07-15 21:27:22 +0530

    Add files via upload

---
+++ Q3/JQ3.java
@@ -0,0 +1,12 @@
+package com.mycompany.jq3;
+public class JQ3
+{
+    public static void main(String[] args)
+    {
+        float x = 13.05f;
+        float y ;
+        System.out.println(" Fahrenheit degree is "+x);
+        y = 5*(x-32)/9;
+        System.out.println(" Celsius degree of Fahrenheit degree is "+y);
+    }
+}
